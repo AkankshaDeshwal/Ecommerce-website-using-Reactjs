ca09c63dda77d1526cd7e64574d957f8163de353
AkankshaDeshwal <adeshwal85@gmail.com>
2026-06-19 15:50:22 +0530

    chore:Setup website routes with skeleton pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import { useState } from 'react'
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Auth from './pages/Auth'
+import Checkout from './pages/Checkout'
 
 function App() {
   
 
   return (
-    <>
-      
-    </>
+  
+      <div>
+          <Routes>
+            <Route path='/' element={<Home/>} />
+            <Route path='/auth' element={<Auth />} />
+            <Route path='/checkout' element={<Checkout />} />
+          </Routes>
+      </div>
+    
   )
 }
 
---
+++ src/pages/Checkout.jsx
@@ -0,0 +1,7 @@
+const Checkout = () => {
+    return ( 
+        <div>Checkout page</div>
+     );
+}
+ 
+export default Checkout;
