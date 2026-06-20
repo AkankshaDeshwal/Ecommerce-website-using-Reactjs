db01f53e535a0af14f40e4c16af3b287da4b7f2a
AkankshaDeshwal <adeshwal85@gmail.com>
2026-06-20 11:56:19 +0530

    style:Implement passing additional styles as props to button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
-const Button = ({btnText}) => {
+const Button = ({btnText, addStyles=""}) => {
     return ( 
-        <button className="p-2 rounded-sm bg-primary hover:bg-secondary cursor-pointer">{btnText}</button>
+        <button className={`p-2 rounded-sm bg-primary hover:bg-secondary cursor-pointer ${addStyles}`}>{btnText}</button>
      );
 }
  
