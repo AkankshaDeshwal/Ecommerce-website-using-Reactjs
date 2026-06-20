1fd004b5d3ab6519c9ce33cb714e327a492ee82a
AkankshaDeshwal <adeshwal85@gmail.com>
2026-06-20 11:57:44 +0530

    style:Add page styles to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,22 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Auth from './pages/Auth'
 import Checkout from './pages/Checkout'
+import Header from './components/Header'
 
 function App() {
   
 
   return (
   
-      <div>
-          <Routes>
+      <div className='flex flex-col items-center w-full bg-background'>
+        <Header />
+          <div className='flex justify-center items-center  w-full md:w-2/3  p-4'>
+            <Routes>
             <Route path='/' element={<Home/>} />
             <Route path='/auth' element={<Auth />} />
             <Route path='/checkout' element={<Checkout />} />
           </Routes>
+          </div>
       </div>
     
   )
