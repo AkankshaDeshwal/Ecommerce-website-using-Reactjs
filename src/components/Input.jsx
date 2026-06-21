a05fce00bedfde5fc61f356c76858e6c69a287ea
AkankshaDeshwal <adeshwal85@gmail.com>
2026-06-21 16:42:57 +0530

    feat:Create reusable Input component

---
+++ src/components/Input.jsx
@@ -0,0 +1,11 @@
+const Input = ({inputLabel, inputType, inputId, inputPlaceholder, onChangeHandler, error}) => {
+    return ( 
+        <div className="flex flex-col items-start gap-2 ">
+            <label htmlFor={inputId}>{inputLabel}</label>
+            <input type={inputType} id={inputId} placeholder={inputPlaceholder} onChange={(e) => onChangeHandler(e, inputId)} className="outline-1 outline-input-outline focus:outline-input-focus px-4 py-2 rounded-sm w-full"/>
+            <p className="text-sm text-red-700">{error}</p>
+        </div>
+     );
+}
+ 
+export default Input;
