fa4d63c144229cb40b7ef15f1d652ad11d447c81
AkankshaDeshwal <adeshwal85@gmail.com>
2026-06-22 20:31:37 +0530

    feat:Implement sorting by user choice

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ const Home = () => {
     const [products, setProducts] = useState([])
     const [isLoading, setIsLoading] = useState(true)
     const [filteredList, setFilteredList] = useState([])
+    const [sortBy, setSortBy] = useState()
 
     const loadProducts = async () => {
         try {
@@ -24,6 +25,35 @@ const Home = () => {
         loadProducts()
     }, [])
 
+    const calculateSellingPrice = (price, disc) => {
+        return (price*(100-disc)/100).toFixed(2)
+    }
+    const sortProducts = () => {
+        let filtered = [...products]
+
+        filtered.sort((a,b) => {
+            let a_sp = calculateSellingPrice(a.price, a.discountPercentage)
+            let b_sp = calculateSellingPrice(b.price, b.discountPercentage)
+
+            switch(sortBy) {
+            case 'price_inc':
+                return a_sp - b_sp 
+            case 'price_dec':
+                return b_sp - a_sp
+            case 'rating':
+                return b.rating - a.rating 
+            default:
+                return a.id - b.id
+        }
+        })
+
+        setFilteredList(filtered)
+    }
+
+    useEffect(() => {
+        sortProducts()
+    }, [sortBy, products])
+
     return ( 
         <div className="flex flex-col gap-6 justify-center py-8">
             <div className='text-center'>
@@ -35,8 +65,8 @@ const Home = () => {
                 <h1 className='text-lg'>Shop</h1>
 
                 <div className=' w-full flex justify-between items-center '>
-                    <select name='sortBy' id='sort'>
-                        <option value='' >Sort by:</option>
+                    <select name='sortBy' id='sort' onChange={(e) => setSortBy(e.target.value)}>
+                        <option value="" >Sort by:</option>
                         <option value='price_inc' >Price(low to high)</option>
                         <option value='price_dec' >Price(high to low)</option>
                         <option value='rating' >Rating(high to low)</option>
@@ -45,7 +75,7 @@ const Home = () => {
                     <input type="text" placeholder='Search products...' className='px-6 py-2 rounded-sm bg-surface focus:outline focus:outline-muted' />
                 </div>
                 
-                {isLoading? <div>Loading...</div> : <div className='w-full grid grid-cols-3 gap-12'>{products.map(product => <ProductCard key={product.id} product={product} />)}</div>}
+                {isLoading? <div>Loading...</div> : <div className='w-full grid grid-cols-3 gap-12'>{filteredList.map(product => <ProductCard key={product.id} product={product} />)}</div>}
             </div>
         </div>
      );
