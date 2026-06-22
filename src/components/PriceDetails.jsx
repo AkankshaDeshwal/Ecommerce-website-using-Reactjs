e66d936adc2d609a08d74755f7482edd73151be7
AkankshaDeshwal <adeshwal85@gmail.com>
2026-06-22 17:03:59 +0530

    style:Set styles of this component according to display location

--- src/components/PriceDetails.jsx
+++ src/components/PriceDetails.jsx
@@ -1,16 +1,18 @@
-const PriceDetails = ({price, discountPercentage}) => {
+const PriceDetails = ({price, discountPercentage, displayLoc}) => {
+
+    const styling = displayLoc === 'product page'?{sellingPrice:"text-xl", price:"text-sm", discountPercentage:"text-sm font-semibold"}: {sellingPrice:"text-lg", price:"text-xs", discountPercentage:"text-xs"}
 
     const sellingPrice = (price*(100-discountPercentage)/100).toFixed(2)
 
     return ( <div className='flex justify-start items-end gap-2'>
                     <div>
-                        <h1 className='text-lg font-bold'>$ {sellingPrice}</h1>
+                        <h1 className={`${styling.sellingPrice} font-bold`}>$ {sellingPrice}</h1>
                     </div>
                     <div>
-                        <h2 className='text-xs line-through mb-0.5'>$ {price.toFixed(2)}</h2>
+                        <h2 className={`${styling.price} font-bold line-through mb-0.5`}>$ {price.toFixed(2)}</h2>
                     </div>
                     <div>
-                        <h1 className='text-xs text-secondary mb-0.5 font-semibold'>({discountPercentage}% OFF)</h1>
+                        <h1 className={`${styling.discountPercentage} font-semibold mb-0.5 text-secondary`}>({discountPercentage}% OFF)</h1>
                     </div>
                 </div> );
 }
