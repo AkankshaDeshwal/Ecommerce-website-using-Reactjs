4d1d9d0870c36c3fd6832b309809aed31a01d612
AkankshaDeshwal <adeshwal85@gmail.com>
2026-06-22 17:05:19 +0530

    fix:Add button handler for onClick

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
-const Button = ({btnText, addStyles=""}) => {
+const Button = ({btnText, addStyles="", btnHandler}) => {
     return ( 
-        <button className={`py-2 px-4 rounded-sm bg-primary hover:bg-secondary cursor-pointer ${addStyles}`}>{btnText}</button>
+        <button className={`py-2 px-4 rounded-sm bg-primary hover:bg-secondary cursor-pointer ${addStyles}`} onClick={btnHandler}>{btnText}</button>
      );
 }
  
