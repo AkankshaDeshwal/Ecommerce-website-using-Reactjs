2cc66f84852a5ea8670f2c003f791a1544b5c9e6
AkankshaDeshwal <adeshwal85@gmail.com>
2026-06-22 17:06:47 +0530

    feat:Implement fetch product details by id

--- src/api/DummyJson.js
+++ src/api/DummyJson.js
@@ -9,3 +9,13 @@ export const fetchProducts = async () => {
 
     return res.json()
 }
+
+export const fetchProductById = async (id) => {
+    const res = await fetch(`${BASE_URL}/${id}`)
+
+    if (!res.ok) {
+        throw new Error('Failed to this product')
+    }
+
+    return res.json()
+}
